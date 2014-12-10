ba33ca4796fd5c918f24709cdfe44e75efba9a52
Ben Raymond <ben.raymond@aad.gov.au>
2014-12-10 14:21:24 +1100

    updated rapid_response because the remote server does not support timestamping

--- R/rapid_response_handler.R
+++ R/rapid_response_handler.R
@@ -12,6 +12,10 @@ rapid_response=function(dataset) {
 
     ## simple loop from current day back to 2013 day 345 (Dec 11)
 
+    ## note some restrictions with the NASA server: it does not provide timestamps nor file sizes in the HTTP headers
+    ## so we can't do timestamping or selective retrieval based on file sizes
+    ## if clobber setting is 0 (don't replace existing files) or 1 (only replace existing files if remote version is newer than local) then existing local files will not be updated. If clobber==2 then existing files get replaced unconditionally
+
     this_resolution="4km"
     d=Sys.Date()
     while (d>=format.Date("2013-12-11")) {
@@ -22,18 +26,18 @@ rapid_response=function(dataset) {
 }
 
 rapid_response_do_download=function(d,platform,resolution,dataset) {
-            this_url=sprintf("http://lance-modis.eosdis.nasa.gov/imagery/subsets/?mosaic=Antarctica.%s%s.%s.%s.tif",format(d,"%Y"),format(d,"%j"),platform,resolution)
-        if (dataset$skip_downloads) {
-            cat(sprintf("not downloading %s, skip_downloads is TRUE\n",this_url))
-        } else {
-            dummy=dataset
-            dummy$method_flags=paste("--progress=dot:giga","--recursive",sep=" ")
-            ## this gives output filenames like "index.html@mosaic=Antarctica.2014003.terra.4km.tif" - would prefer these to be just "Antarctica.*"
-            ##dummy$method_flags=paste("--progress=dot:giga","--directory-prefix=lance-modis.eosdis.nasa.gov/imagery/subsets/",paste0("--output-document=",sub("^http.*mosaic=","",this_url)),sep=" ")
-            ## can't use --output-document option with timestamping, so it looks like we have to put up with odd filenames
-            dummy$source_url=this_url
-            wget_call=build_wget_call(dummy)
-            do_wget(wget_call,dataset)
-        }
+    this_url=sprintf("http://lance-modis.eosdis.nasa.gov/imagery/subsets/?mosaic=Antarctica.%s%s.%s.%s.tif",format(d,"%Y"),format(d,"%j"),platform,resolution)
+    dummy=dataset
+    ##dummy$method_flags=paste("--progress=dot:giga","--recursive",sep=" ")
+    ## this gives output filenames like "index.html@mosaic=Antarctica.2014003.terra.4km.tif" - would prefer these to be just "Antarctica.*"
+    ## note that we can't use --output_document option with --timestamping, but since the server doesn't support timestamping
+    dummy$method_flags=paste("--progress=dot:giga",paste0("--output-document=lance-modis.eosdis.nasa.gov/imagery/subsets/",sub("^http.*mosaic=","",this_url)),sep=" ")
+    if (file.exists(paste0("lance-modis.eosdis.nasa.gov/imagery/subsets/",sub("^http.*mosaic=","",this_url))) & (dataset$clobber<2)) {
+        cat(sprintf("not downloading %s, local file exists and clobber setting is <2\n",this_url))
+    } else {
+        dummy$source_url=this_url
+        wget_call=build_wget_call(dummy)
+        do_wget(wget_call,dataset)
+    }
 }
 
