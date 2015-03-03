d57618489f60ea03944f6137cab2282eef6413c2
Ben Raymond <ben.raymond@aad.gov.au>
2015-03-03 12:11:34 +1100

    added repo_summary function and knitr dependency

--- R/raadsync.R
+++ R/raadsync.R
@@ -4,6 +4,6 @@
 #' @author Australian Antarctic Data Centre \email{aadc@@aad.gov.au}
 #' @docType package
 #' @references \url{http://www1.data.antarctica.gov.au}
-#' @import digest jsonlite stringr plyr assertthat
+#' @import digest jsonlite stringr plyr assertthat knitr
 #' @importFrom R.utils gunzip bunzip2
 NULL
