011f209aee261cb4c0f40e52b09334de474897f2
Michael Sumner <mdsumner@gmail.com>
2016-09-06 20:33:45 +1000

    scaffolding

--- tests/testthat/test-repo-summary.R
+++ tests/testthat/test-repo-summary.R
@@ -3,10 +3,9 @@ library(raadsync)
 
 context("repo summary")
 
-cf <- read_repo_config(local_config_file = NULL)
-## cannot get this to work?  Windows problem, or something old? 
-#Error in knit2html(rmd_file, output = sub("Rmd$", "md", rmd_file)) : 
-#  It seems you should call rmarkdown::render() instead of knitr::knit2html() because C:\Users\mdsumner\AppData\Local\Temp\RtmpqKvoje\filef3c4a177756.Rmd appears to be an R Markdown v2 document.
-#test_that("repo summary is sensible", {
- # repo_summary(cf)
-#})
+
+test_that("repo summary is sensible", {
+
+   expect_that(  cf <- read_repo_config(local_config_file = NULL), is_a("data.frame"))
+  expect_that(all(cf$do_sync), is_false())
+})
