55b09bb5846a0d815f42007084a2d13af516a165
Michael Sumner <mdsumner@gmail.com>
2016-09-06 20:57:32 +1000

    some more tests

--- tests/testthat/test-repo-summary.R
+++ tests/testthat/test-repo-summary.R
@@ -1,11 +1,11 @@
-library(testthat)
-library(raadsync)
 
 context("repo summary")
 
 
 test_that("repo summary is sensible", {
 
-   expect_that(  cf <- read_repo_config(local_config_file = NULL), is_a("data.frame"))
+  expect_that(  cf <- read_repo_config(local_config_file = NULL), is_a("data.frame"))
+  
+  expect_that(file.exists(repo_summary(cf)), is_true())
   expect_that(all(cf$do_sync), is_false())
 })
