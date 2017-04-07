6afb16da62bf0a08b17762182aa39a5e8f69abe4
Michael Sumner <mdsumner@gmail.com>
2017-04-07 11:44:45 +1000

    duh

---
+++ tests/testthat/test-sync.R
@@ -0,0 +1,20 @@
+context("sync")
+
+cf <- read_repo_config(local_config_file = NULL)
+cf <- subset(cf, name == "NSIDC SMMR-SSM/I Nasateam sea ice concentration")
+cf$do_sync <- TRUE
+tok <- "--accept=\"*nt_200703*\""
+cf$method_flags <- paste(cf$method_flags, "--accept=\"*nt_2016*\"", "--accept=\"*nt_2015*\"")
+
+lfr <- tempdir()
+cf$local_file_root <- lfr
+
+test_that("sync  works", {
+  expect_true(sync_repo(cf))
+})
+
+test_that("sync got some files", {
+  files <- data.frame(fullname = file.path(lfr, list.files(lfr, recursive = TRUE)), stringsAsFactors = FALSE)
+  expect_gt(nrow(files), 0L)
+})
+
