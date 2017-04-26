2012197d484ec5554ed3b2f0891d390136f3c3d5
Ben Raymond <ben.raymond@aad.gov.au>
2017-04-27 09:49:13 +1000

    ghrsst handler updated to new root URL

--- R/ghrsst_handler.R
+++ R/ghrsst_handler.R
@@ -1,9 +1,9 @@
-ghrsst=function(dataset) {
+ghrsst <- function(dataset) {
     ## ghrsst synchronisation handler
 
-    ## The data source is ftp://ftp.nodc.noaa.gov/pub/data.nodc/ghrsst/L4/GLOB/JPL/MUR/
-    ## with yearly subdirectories ftp://ftp.nodc.noaa.gov/pub/data.nodc/ghrsst/L4/GLOB/JPL/MUR/2015/
-    ## within yearly directories, days are subdirectories ftp://ftp.nodc.noaa.gov/pub/data.nodc/ghrsst/L4/GLOB/JPL/MUR/2015/051/ with actual data files therein
+    ## The data source is ftp://podaac-ftp.jpl.nasa.gov/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/
+    ## with yearly subdirectories
+    ## within yearly directories, days are subdirectories
     ## BUT the daily subdirectories are symlinks and recursive wget won't recurse symlinked directories (known limitation of wget)
 
     ## hence use a custom handler, at least for now
@@ -12,31 +12,33 @@ ghrsst=function(dataset) {
     ## wget --recursive --level=inf --no-parent --timestamping ftp://ftp.nodc.noaa.gov/pub/data.nodc/ghrsst/L4/GLOB/JPL/MUR/2015/051/
 
     ## we expect that the provided source URL is either pointing to the root of the collection:
-    ## ftp://ftp.nodc.noaa.gov/pub/data.nodc/ghrsst/L4/GLOB/JPL/MUR/
+    ## ftp://podaac-ftp.jpl.nasa.gov/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/
     ## or to a particular year (multiple years will need multiple source_urls)
-    ## ftp://ftp.nodc.noaa.gov/pub/data.nodc/ghrsst/L4/GLOB/JPL/MUR/2015/
+    ## ftp://podaac-ftp.jpl.nasa.gov/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/2015/
     ##
     ## for the former, we will loop from 2002 to present here
     ## for the latter, just hit that specific yearly directory
 
-    if (grepl("JPL/MUR/?$",dataset$source_url)) {
+    if (!grepl("\\d\\d\\d\\d/?$",dataset$source_url)) {
         ## pointing to root
-        yearlist=seq(from=2002,to=as.numeric(format(Sys.Date(),"%Y")),by=1)
+        yearlist <- seq(from=2002,to=as.numeric(format(Sys.Date(),"%Y")),by=1)
     } else {
-        yearlist=as.numeric(basename(dataset$source_url))
+        yearlist <- as.numeric(basename(dataset$source_url))
     }
+    yearlist <- na.omit(yearlist)
+    if (length(yearlist)<1) warning("ghrsst: empty yearlist")
     ## make sure method_flags include --recursive --no-parent
     if (!grepl("--recursive",dataset$method_flags,ignore.case=TRUE)) {
-        dataset$method_flags=paste(dataset$method_flags,"--recursive",sep=" ")
+        dataset$method_flags <- paste(dataset$method_flags,"--recursive",sep=" ")
     }
     if (!grepl("--no-parent",dataset$method_flags,ignore.case=TRUE)) {
-        dataset$method_flags=paste(dataset$method_flags,"--no-parent",sep=" ")
+        dataset$method_flags <- paste(dataset$method_flags,"--no-parent",sep=" ")
     }
     for (thisyear in yearlist) {
         for (thisday in 1:366) {
-            dummy=dataset
-            dummy$source_url=paste0("ftp://podaac-ftp.jpl.nasa.gov/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/",thisyear,"/",sprintf("%03d",thisday),"/")
-            wget_call=build_wget_call(dummy)
+            dummy <- dataset
+            dummy$source_url <- paste0("ftp://podaac-ftp.jpl.nasa.gov/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/",thisyear,"/",sprintf("%03d",thisday),"/")
+            wget_call <- build_wget_call(dummy)
             do_wget(wget_call,dataset)
         }
     }
@@ -44,7 +46,7 @@ ghrsst=function(dataset) {
 
 
 
-ghrsst2 <- 
+ghrsst2 <-
 function (dataset)
 {
     if (grepl("JPL/MUR/?$", dataset$source_url)) {
